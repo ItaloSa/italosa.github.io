f14c77924f1ad8bc7ee30a2e6bc788d05ffa0031
Italo Sousa <italusousa@gmail.com>
2021-03-16 12:37:54 -0300

    build

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8baf72e3c82337b9df10";
+/******/ 	var hotCurrentHash = "4636ad7a94093c9f2878";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
