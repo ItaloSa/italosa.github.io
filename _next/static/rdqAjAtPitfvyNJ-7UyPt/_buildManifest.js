e2e7dc8bc3bf49635dfad492f4f90e3ef60cf598
Italo Sousa <italusousa@gmail.com>
2024-12-13 15:35:30 -0300

    build

---
+++ _next/static/rdqAjAtPitfvyNJ-7UyPt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-9a83484e4ebc59a0.js","static/chunks/683-6948d36a6d4b5ea2.js","static/chunks/pages/index-9f4dc028f0638913.js"],"/_error":["static/chunks/pages/_error-dfe71f0360f7207a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
